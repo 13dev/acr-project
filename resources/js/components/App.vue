0df353196d7fea2bd31258f2996b11ddda10d2ce
Leo Oliveira <qwerty124563@gmail.com>
2020-12-06 19:22:59 +0000

    Add sidebar

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,15 +1,29 @@
 <template>
-    <p>
-        Ola
-    </p>
+    <div class="bg-gray-900 min-h-screen h-screen overflow-hidden font-sans flex flex-col text-gray-400">
+        <div class="flex flex-1">
+            <sidebar></sidebar>
+
+            <div class="flex-1 relative overflow-y-scroll">
+                <!-- search -->
+
+                <div class="p-6 flex justify-center overflow-y-scroll">
+                    <div class="w-full">
+                        <router-view></router-view>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- player -->
+    </div>
 </template>
 
 <script>
+import Sidebar from "./Sidebar";
+
 export default {
-    name: "App"
+    name: 'app',
+    components: {
+        Sidebar
+    }
 }
 </script>
-
-<style scoped>
-
-</style>
