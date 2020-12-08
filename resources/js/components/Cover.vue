47b31b92bc12b4b45af4e2d47d7f3ddaf406184f
Leo Oliveira <qwerty124563@gmail.com>
2020-12-08 21:34:59 +0000

    Add cover

---
+++ resources/js/components/Cover.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div v-if="!song" class="w-full block bg-dark flex items-center justify-center text-5xl" style="height: 200px">
+            <i class="fas fa-compact-disc fa-3x text-gray-800 fa-spin"></i>
+        </div>
+
+        <img v-else :src="'/' + song.album.cover" :alt="song.album.name" class="h-full mr-6"
+             style="width: 220px; max-height: 220px;">
+    </div>
+</template>
+
+<script>
+import {mapActions, mapGetters} from 'vuex'
+
+export default {
+    name: 'Cover',
+    computed: {
+        ...mapGetters('player', [
+            'song'
+        ]),
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
