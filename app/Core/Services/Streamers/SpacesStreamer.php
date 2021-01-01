a20e81860fb1918df7f974b5fa783a4d7f53a042
Leo Oliveira <qwerty124563@gmail.com>
2021-01-01 22:35:51 +0000

    Clean streamer and change login and register

--- app/Core/Services/Streamers/SpacesStreamer.php
+++ app/Core/Services/Streamers/SpacesStreamer.php
@@ -5,8 +5,6 @@ namespace App\Core\Services\Streamers;
 
 use Carbon\Carbon;
 use GuzzleHttp\Stream\Stream;
-use Illuminate\Http\File;
-use Illuminate\Support\Str;
 use Storage;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,34 +12,26 @@ use Response as ResponseAlias;
 
 class SpacesStreamer extends Streamer
 {
-
-
     public function stream(): Response
     {
-        $filePath = config('songs.spaces_path') . $this->song->path;
+        BinaryFileResponse::trustXSendfileTypeHeader();
+
         $disk = Storage::disk('spaces');
+        $filePath = config('songs.spaces_path') . $this->song->path;
         $path = $disk->temporaryUrl($filePath, Carbon::now()->addMinutes(5));
 
-        $name = basename($filePath);
-        $size = $disk->getSize($filePath);
-        $type = 'audio/mp3';
-
-        return ResponseAlias::streamed($type, $size, $name, function($offset, $length) use ($path) {
-            $stream = Stream::factory(fopen($path, 'rb'));
-            $stream->seek($offset);
-            while (!$stream->eof()) {
-                echo $stream->read($length);
-            }
-            $stream->close();
-        });
-
-//        BinaryFileResponse::trustXSendfileTypeHeader();
-//
-//
-//
-//        return Storage::disk('spaces')->download($filePath, null, [
-//            'Accept-Ranges' =>'bytes'
-//        ]);
+        return ResponseAlias::streamed(
+            $this->contentType,
+            $disk->getSize($filePath),
+            basename($filePath),
+            function ($offset, $length) use ($path) {
+                $stream = Stream::factory(fopen($path, 'rb'));
+                $stream->seek($offset);
+                while (!$stream->eof()) {
+                    echo $stream->read($length);
+                }
+                $stream->close();
+            });
 
     }
 }
