37c234d841815700a2a2396b76e1267646b32746
Leo Oliveira <qwerty124563@gmail.com>
2021-01-04 16:25:50 +0000

    remove service provider

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            Albums
         </h2>
     </x-slot>
 
@@ -9,7 +9,41 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-md sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <table class="border-collapse w-full">
+                        <thead>
+                        <tr>
+                            <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">ID</th>
+                            <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">Name</th>
+                            <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">Artist</th>
+                            <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        @foreach($albums as $album)
+                        <tr class="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0">
+                            <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+                                {{ \Illuminate\Support\Str::limit($album->id, 8)  }}
+                            </td>
+                            <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
+                                {{ $album->name }}
+                            </td>
+                            <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
+                                {{ $album->artist->name }}
+                            </td>
+                            <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
+                                <a href="{{ route('dashboard.album-edit-view', ['album' => $album->id]) }}" class="text-blue-400 hover:text-blue-600 underline">Edit</a>
+
+                                <form method="POST" action="{{ route('dashboard.album-delete',['album' => $album->id]) }}">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button type="submit" class="text-blue-400 hover:text-blue-600 underline pl-6">Remove</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
